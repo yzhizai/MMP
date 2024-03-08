7cadab537d1262e5bc88e9b541e233a318aec65f
yzhizai <dsf_ihep@yeah.net>
2024-03-08 22:59:24 +0800

    reformat the code

--- R/assist_func_file.R
+++ R/assist_func_file.R
@@ -4,6 +4,32 @@ library(MOVICS)
 library(class)
 
 
+#' Convert the file to the datalist
+#'
+#' @param flist a vector containing the names of the feature files
+#' @param idx the indices of row to use.
+#'
+#' @return a data list
+#' @export
+#'
+#' @examples
+getDatalist <- function(flist, idx)
+{
+  src.info <- read_csv(src.info.file)
+  dt.list <- list()
+  for(fname in flist)
+  {
+    dt.i <- read_csv(fname) %>% select(-1)
+    baseName <- str_replace(fname, '.csv', '')
+
+    dt.i.train <- dt.i[idx, ]
+
+    dt.list[[baseName]] <- dt.i.train
+  }
+
+  dt.list
+}
+
 #' This function is used for dimension reduction for both training and test datasets, if dt.list.test is null
 #' only output training dataset.
 #'
@@ -43,7 +69,7 @@ DR_umap <- function(dt.list.train, dt.list.test = NULL)
 
   if(is.null(dt.list.test))
   {
-    return(clust.data)
+    return(list(clust.data))
   } else {
     return(list(clust.data, clust.data.test))
   }
@@ -111,4 +137,61 @@ knn_train_test <- function(cmoic.clust, clust.data.train, clust.data.test)
   cl.test <- knn(dt.train, dt.test, cl, k = 3, prob = F)
 
   return(cl.test)
+}
+
+#' Draw the Heatmap to display the feature distribution.
+#'
+#' @param clust.data The datalist containing data.list.train, dt.list.test
+#' @param out.clust The prediction cluster results
+#' @param fig.name The name of heatmap
+#'
+#' @return
+#' @export
+#'
+#' @examples
+draw_Heatmap <- function(clust.data, out.clust, fig.name = 'Heatmap')
+{
+  plotdata <- getStdiz(data       = clust.data,
+                       halfwidth  = rep(2, times = length(clust.data)), # no truncation for mutation
+                       centerFlag = rep(T, times = legnth(clust.data)), # no center for mutation
+                       scaleFlag  = rep(T, times = legnth(clust.data))) # no scale for mutation
+
+  a.col   <- c("#00FF00", "#008000", "#000000", "#800000", "#FF0000")
+  b.col <- c("#0074FE", "#96EBF9", "#FEE900", "#F00003")
+
+  col.list <- list()
+
+  for(i.a in 1:length(clust.data))
+  {
+    if(i.a %% 2 == 0)
+    {
+      col.list[[i.a]] <- a.col
+    }else {
+      col.list[[i.a]] <- b.col
+    }
+  }
+
+  # feat   <- cmoic.clust$feat.res
+  # feat1  <- feat[which(feat$dataset == "static"),][1:10,"feature"]
+  # feat2  <- feat[which(feat$dataset == "dynamic"),][1:10,"feature"]
+  #
+  # annRow <- list(feat1, feat2)
+
+  # comprehensive heatmap (may take a while)
+  getMoHeatmap(data          = plotdata,
+               row.title     = names(clust.data),
+               is.binary     = rep(F, length(clust.data)), # the 4th data is mutation which is binary
+               legend.name   = names(clust.data),
+               clust.res     = out.clust, # cluster results
+               clust.dend    = NULL, # no dendrogram
+               show.rownames = rep(F, length(clust.data)), # specify for each omics data
+               show.colnames = FALSE, # show no sample names
+               # annRow        = annRow, # mark selected features
+               color         = col.list,
+               annCol        = NULL, # no annotation for samples
+               annColors     = NULL, # no annotation color
+               width         = 10, # width of each subheatmap
+               height        = 5, # height of each subheatmap
+               fig.name      = fig.name)
+
 }
---
+++ demo.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(umap)
+library(MOVICS)
+library(class)
+
+flist <- c('feature_static_all.csv',
+           'feature_dynamic_all.csv')
+src.info <- read_csv('data_from_source.csv')
+
+dt.list.train <- getDatalist(flist, which(src.info$Source == 0))
+dt.list.test <- getDatalist(flist, which(src.info$Source == 1))
+# dimension reduction.
+clust.data <- DR_umap(dt.list.train = dt.list.train, dt.list.test = dt.list.test)
+
+# clustering analysis
+cmoic.clust <- clust_identify(clust.data[[1]])
+
+# predict the cluster
+test.pred <- knn_train_test(cmoic.clust,
+               clust.data.train = clust.data[[1]],
+               clust.data.test = clust.data[[2]])
+
+src.info$Group <- test.pred
+
+out.clust <- data.frame(samID = paste0('subj', 1:length(test.pred)),
+                    clust = test.pred)
+
+write_csv(src.info, 'grp_info.csv')
+
+out.clust.validation <- out.clust %>% slice(which(src.info$Source == 1)) %>%
+  as.data.frame
+rownames(out.clust.validation) <- out.clust.validation$samID
+
+clust.data.2 <- clust.data[[2]]
+draw_Heatmap(clust.data.2, out.clust.validation, fig.name = 'Heatmap')
+
+
